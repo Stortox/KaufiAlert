1baf5214cceb71e7310a5f414e7923be21844d4d
Stortox <daniel.musch@hotmail.com>
2025-07-29 12:19:20 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# kaufi_allert_v2
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/kaufi_allert_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kaufi_allert_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
